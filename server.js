40365d5a1bc6dcd18c9ba17231edb7838cee7472
Janitha Tennakoon <janitha000@gmail.com>
2020-08-19 14:09:53 +0800

    Added ejs

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+
+const server = require('http').Server(app);
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('room')
+})
+
+
+server.listen(3000, () => {
+    console.log('server is running...')
+})
+
